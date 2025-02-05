cfa95eea579f2415581d66615d801b8c5ad0ef4d
Syed Sharuk <Sharuk.syed@yahoo.com>
2025-02-05 23:29:34 +0800

    Add project files.

---
+++ Make-My-Trip/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Make_My_Trip.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
